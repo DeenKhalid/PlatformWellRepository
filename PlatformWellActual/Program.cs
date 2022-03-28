970db1777cca7ca5775866f9503e6cbf99ea22fe
DeenKhalid <102508244+DeenKhalid@users.noreply.github.com>
2022-03-28 15:28:37 +0800

    Add files via upload

---
+++ PlatformWellActual/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PlatformWellActual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BusinessLogic.ProcessAPI processAPI = new BusinessLogic.ProcessAPI();
+            processAPI.Entry();
+        }
+    }
+}
